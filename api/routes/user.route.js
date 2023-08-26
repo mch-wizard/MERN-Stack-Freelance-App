d2054ffcac1b9460fb59d103ccbebce0c7797576
Marcin <mch.wizard@gmail.com>
2023-08-26 23:30:44 +0200

    added JWT, cookie-parser, login, deleteUser, middleware - verifyToken function, utils - createError function for next()

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { deleteUser, getUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
+
 
 const router = express.Router();
 
-router.get("")
+router.delete("/:id", verifyToken, deleteUser);
+router.get("/:id", getUser);
 
 export default router;
