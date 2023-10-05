e78c88cec28e6b15bcd1ecf0c3ce208e78c61f48
Marcin <mch.wizard@gmail.com>
2023-10-05 20:42:48 +0200

    added database integration in MyGigs and Featured components

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import "./Featured.scss";
+import { useNavigate } from "react-router-dom";
 
-function Featured() {
+const Featured = () => {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate(`/gigs?search=${input}`);
+  };
+
   return (
     <div className="featured">
       <div className="container">
@@ -11,9 +20,13 @@ function Featured() {
           <div className="search">
             <div className="searchInput">
               <img src="./img/search.png" alt="" />
-              <input type="text" placeholder='Try "building mobil app"' />
+              <input
+                type="text"
+                placeholder='Try "building mobil app"'
+                onChange={(e) => setInput(e.target.value)}
+              />
             </div>
-            <button>Search</button>
+            <button onClick={handleSubmit}>Search</button>
           </div>
           <div className="popular">
             <span>Popular:</span>
